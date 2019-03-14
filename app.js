fb5a89f052988b680db9fee165472f22e389363c
hxdegawa <hxdegawa@gmail.com>
2019-03-14 19:34:58 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+client.login(process.env.BOT_TOKEN);
